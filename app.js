86e59f4b1ddacc47f0fe3e4e865b7791ddd26314
Tariquzzaman-faisal <tariquzzamanfaisal1964@gmail.com>
2021-03-27 21:48:02 +0600

    Users can publish and view secrets, but only login via gmail works

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -85,12 +86,44 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            res.send(err);
+        } else {
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            } else {
+                res.send("No secret found!");
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
 });
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    console.log(req.user.id);
+    User.findById(req.user.id, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
 
 app.get("/logout", function (req, res) {
     req.logout();
